10a5841af12cc341fedb0ae98c168bc16593fd89
alvarofpp <alvarofepipa@gmail.com>
2020-12-18 02:48:50 -0300

    refactor: Parse

--- tasks/task-03/utils/Parse.py
+++ tasks/task-03/utils/Parse.py
@@ -15,12 +15,12 @@ class Parse:
             'rho': None,
             'theta': None,
             'threshold': None,
+            'take': None,
         }
         self.filename = sys.argv[Parse.INIT - 1]
 
-
         argv = sys.argv[Parse.INIT:]
-        opts, args = getopt.getopt(argv, '-h', ['help', 'rho=', 'theta=', 'threshold='])
+        opts, args = getopt.getopt(argv, '-h', ['help', 'rho=', 'theta=', 'threshold=', 'take=',])
         self._get_opts(opts)
         self._get_args(args)
 
@@ -30,8 +30,13 @@ class Parse:
 
         return None
 
-    def get_valid_parameters(self):
-        return {key: value for key, value in self.params.items() if value is None}
+    def get_valid_parameters(self, context='accumulator'):
+        keys = ['rho', 'theta',]
+
+        if context == 'detector':
+            keys = ['threshold', 'take',]
+
+        return {key: value for key, value in self.params.items() if value is not None and key in keys}
 
     def _help(self):
         print('Exemplos de uso:')
@@ -44,6 +49,7 @@ class Parse:
         print('    --rho=<valor para rho>')
         print('    --theta=<valor para theta>')
         print('    --threshold=<valor para threshold>')
+        print('    --take=<valor que limita a quantidade de linhas> (Esse parâmetro excluí o threshold)')
         pass
 
     def _get_opts(self, opts):
@@ -52,7 +58,7 @@ class Parse:
                 self._help()
                 sys.exit()
 
-            self.params[opt[2:-1]] = arg
+            self.params[opt[2:-1]] = int(arg)
 
     def _get_args(self, args):
         keys_empty = [key for key, value in self.params.items() if value is None]
@@ -61,4 +67,4 @@ class Parse:
             if len(keys_empty) == 0:
                 pass
 
-            self.params[keys_empty.pop(0)] = arg
+            self.params[keys_empty.pop(0)] = int(arg)
